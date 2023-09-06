e73f7a80dcb9ba1fb4933b59d63ece55b9df552d
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-06 14:43:26 -0400

    Completes 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(str_text, repeat_times) {
+    return repeat_times >= 0 ? 
+        Array(repeat_times).fill(str_text).join(""):
+        "ERROR";
 };
 
 // Do not edit below this line
