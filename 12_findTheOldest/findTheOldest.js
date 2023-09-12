397a5d1af715b0c0803990e1ff9c7ecd159cbf7e
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-12 10:29:10 -0400

    Completes 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const calculateAge = (birth, death) => {
+    if (death === undefined) death = new Date().getFullYear();
+    return death - birth;
+}
+const findTheOldest = (people) => {
+    return people.reduce(
+        (accum, current) => {  
+            if (
+                calculateAge(accum.yearOfBirth, accum.yearOfDeath) > 
+                calculateAge(current.yearOfBirth, current.yearOfDeath)) {
+                return accum;
+            } else {
+                return current;
+            }
+        },
+        {}
+    );
 };
 
 // Do not edit below this line
