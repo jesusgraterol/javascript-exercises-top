70a66cce57a2d3b966d12074e927e8ff5e7adaec
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-12 09:04:42 -0400

    Completes 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+    return values.reduce((accum, current) => accum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+    return values.reduce((accum, current) => accum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+    let res = 1;
+	for (let i = 1; i <= value; i++) res = res * i;
+    return res;
 };
 
 // Do not edit below this line
