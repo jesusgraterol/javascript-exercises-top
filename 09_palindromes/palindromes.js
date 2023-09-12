2d65d26968c7f18aa6123d40ae5119d65b46b346
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-12 09:24:58 -0400

    Completes 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (value) {
+    const clean_value = value.replace(/[.,!/\s]/g, "").toLowerCase();
+    return clean_value == clean_value.split("").reverse().join("");
 };
 
 // Do not edit below this line
