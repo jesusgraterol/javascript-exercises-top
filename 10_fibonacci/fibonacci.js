66a533ec0c941e69c8463dc3247489bc013b5bdc
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-12 10:12:49 -0400

    Completes 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    // Validate/Format the input
+    if (val < 0) return "OOPS";
+    if (typeof val != "number") val = Number(val)
+    
+    // Build the fib sequence
+    let sequence = [0, 1];
+    for (let i = 1; i < val; i++) {
+        sequence.push(sequence.at(-2) + sequence.at(-1));
+    }
+    return sequence.at(-1);
 };
 
 // Do not edit below this line
